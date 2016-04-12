1d697385a00f71023d3127ed98bd4939e031bde1
Billy Hart <bhart12@avc.edu>
2016-04-12 15:55:56 -0700

    Both Initalizers

--- Java/CIS 175/Lab 2/GoLInitializer.java
+++ Java/CIS 175/Lab 2/GoLInitializer.java
@@ -1,10 +1,21 @@
+/*
+ * ############################################################################
+ * Name: Billy Hart
+ * Date: Tue Apr 12 15:48:38 2016
+ * Description: This class implements an interface for initializing a board.
+ * ############################################################################
+*/
 
+// Includes
+import java.util.*;
 
 public class GoLInitializer {
 
-	public static int[][] getCellArray() {
-		int[][] cell = new int[20][20];
-		return cell;
-	}
+    // Method(s) --------------------------------------------------------------
+    
+    // Returns an array of cells
+    public GoLCell[][] getCellArray() {
+        //
+    }
 
 }
---
+++ Java/CIS 175/Lab 2/GoLRandomInitializer.java
@@ -0,0 +1,36 @@
+/*
+ * ############################################################################
+ * Name: Billy Hart
+ * Date: Tue Apr 12 15:51:34 2016
+ * Description: This class implements an interface for initializing a board.
+ * ############################################################################
+*/
+
+// Includes
+import java.util.*;
+
+public class GoLRandomInitializer {
+    
+    // Instance Variables
+    
+
+    // Constructor(s) ---------------------------------------------------------
+
+    // Creates new GoLRandomInitializer with a random seed
+    public GoLRandomInitializer() {
+        //
+    }
+
+    // Creates new GoLRandomInitializer with a given seed
+    public GoLRandomInitializer(long seed) {
+        //
+    }
+
+    // Method(s) --------------------------------------------------------------
+
+    // Returns an array of cells
+    public GoLCell[][] getCellArray() {
+        //
+    }
+
+}
